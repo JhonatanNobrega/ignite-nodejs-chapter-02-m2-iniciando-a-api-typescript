ffb729ff4af60bb8ee4c1755bf444d8dc34035d0
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-03-20 01:22:54 -0300

    Configurando padroes do projeto e ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
+app.listen(3333, () => console.log('Server is running!'));
